669017806a3caaf071217553f4700b46676fd972
CodiceDeploy <jose.cruz@codice.com>
2016-08-09 16:54:03 -0500

    Cambio a ajustes-sin-descarga-playera

--- fuzetea/execs_aws.sh
+++ fuzetea/execs_aws.sh
@@ -2,8 +2,7 @@
 cd /var/www/html/
 echo "Inicio de log script remoto"
 git pull
-#git checkout ajustes-sin-descarga-playera
-git checkout master
+git checkout ajustes-sin-descarga-playera
 git pull
 echo "git status"
 echo ""
