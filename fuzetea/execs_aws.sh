5481ab237e732736fe5097a5c5f171670d2b7010
CodiceDeploy <jose.cruz@codice.com>
2016-08-10 12:55:33 -0500

    Script para descarga de backupuploads - echo "Obtencion del zip de la ruta" - echo "============================" - cd /var/www/html/ - wget -c https://ssl.codice.com/fuzetea_dev/ssluploads.zip - echo "============================" - echo "Fin del ZIP, UNZIP en assets/uploads" - echo "============================" - unzip ssluploads.zip -d /var/www/html/assets/uploads - echo "============================" - echo "FIN UNZIP" - echo "============================" - echo "EOF"

--- fuzetea/execs_aws.sh
+++ fuzetea/execs_aws.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 cd /var/www/html/
-echo "Inicio de log script remoto"
-git pull
+# echo "Inicio de log script remoto"
+# git pull
 # git checkout ajustes-sin-descarga-playera
 # git pull
-echo "git status"
-echo ""
-git status
-echo "git log"
-echo ""
-git log
+# echo "git status"
+# echo ""
+# git status
+# echo "git log"
+# echo ""
+# git log
+echo "Obtencion del zip de la ruta"
+echo "============================"
+cd /var/www/html/
+wget -c https://ssl.codice.com/fuzetea_dev/ssluploads.zip
+echo "============================"
+echo "Fin del ZIP, UNZIP en assets/uploads"
+echo "============================"
+unzip ssluploads.zip -d /var/www/html/assets/uploads
+echo "============================"
+echo "FIN UNZIP"
+echo "============================"
 echo "EOF"
