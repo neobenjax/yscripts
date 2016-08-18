f535fc16dda52d7011b6b817ce2a11f64b4e819a
CodiceDeploy <jose.cruz@codice.com>
2016-08-18 16:10:56 -0500

    Scripts para sta clara

---
+++ staclara/execs_aws.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+cd /var/www/html/
+echo "Inicio de log script remoto"
+#git pull
+#git checkout ajustes-sin-descarga-playera
+#git pull
+echo "git status"
+echo ""
+git status
+echo "git log"
+echo ""
+git log
+echo "============================"
+echo "EOF"
+echo "============================"
